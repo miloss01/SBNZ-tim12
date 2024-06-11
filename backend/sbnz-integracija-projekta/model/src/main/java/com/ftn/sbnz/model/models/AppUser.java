747e8934329f499e178af9d539652c3b8f654449
MilosS019 <milosstanojlovic019@gmail.com>
2024-06-11 15:24:57 +0200

    Added new rules

--- backend/sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/AppUser.java
+++ backend/sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/AppUser.java
@@ -14,69 +14,98 @@ public class AppUser implements Serializable {
     private Double playtime;
     private String timezone;
     private SubscriptionType subscriptionType;
-    private ArrayList<Game> games;
-    private ArrayList<Game> wishlist;
-    private ArrayList<Friend> friends = new ArrayList<>();
-    
+    private ArrayList<Game> games = new ArrayList<>();
+    private ArrayList<Game> wishlist = new ArrayList<>();
+    private ArrayList<AppUser> friends = new ArrayList<>();
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public Double getBalance() {
         return balance;
     }
+
     public void setBalance(Double balance) {
         this.balance = balance;
     }
+
     public ArrayList<String> getFavouriteGenres() {
         return favouriteGenres;
     }
+
     public void setFavouriteGenres(ArrayList<String> favouriteGenres) {
         this.favouriteGenres = favouriteGenres;
     }
+
     public Double getPlaytime() {
         return playtime;
     }
+
     public void setPlaytime(Double playtime) {
         this.playtime = playtime;
     }
+
     public String getTimezone() {
         return timezone;
     }
+
     public void setTimezone(String timezone) {
         this.timezone = timezone;
     }
+
     public SubscriptionType getSubscriptionType() {
         return subscriptionType;
     }
+
     public void setSubscriptionType(SubscriptionType subscriptionType) {
         this.subscriptionType = subscriptionType;
     }
+
     public ArrayList<Game> getGames() {
         return games;
     }
+
     public void setGames(ArrayList<Game> games) {
         this.games = games;
     }
+
     public ArrayList<Game> getWishlist() {
         return wishlist;
     }
+
     public void setWishlist(ArrayList<Game> wishlist) {
         this.wishlist = wishlist;
     }
-    public ArrayList<Friend> getFriends() {
+
+    public ArrayList<AppUser> getFriends() {
         return friends;
     }
-    public void setFriends(ArrayList<Friend> friends) {
+
+    public void setFriends(ArrayList<AppUser> friends) {
         this.friends = friends;
     }
+
+    public AppUser(String username, Double balance, ArrayList<Game> games) {
+        this.username = username;
+        this.balance = balance;
+        this.games = games;
+    }
+
+    public AppUser() {
+    }
+
 
 }
--- backend/sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/Notification.java
+++ backend/sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/Notification.java
@@ -8,4 +8,37 @@ public class Notification implements Serializable {
     private String description;
     private AppUser user;
 
+    public Notification(String name, String description, AppUser user) {
+        this.name = name;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Notification() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public AppUser getUser() {
+        return user;
+    }
+
+    public void setUser(AppUser user) {
+        this.user = user;
+    }
 }
